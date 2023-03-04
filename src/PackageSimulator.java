d46b626c88f5cf39b8780bc2f7e97a885227f227
lasar <lsarmiento9311@bths.edu>
2023-03-04 02:32:05 -0500

    Finished the menu Finished PackageSimulator (this includes methods getSimulationInfo(), resetSimulation(), generateTotalCost(), and generatePackages())

--- src/PackageSimulator.java
+++ src/PackageSimulator.java
@@ -9,34 +9,35 @@ public class PackageSimulator {
 
     public void generatePackages(int numOfPackages) {
         for (int i = 0; i < numOfPackages; i++) {
-            Address origin = new Address("123", "Random Street", "City", "State", 49100);
-            Address destination = new Address("123", "Random Street", "City", "State", 49100);
-            double weight = 0;
-            double length = 0;
-            double width = 0;
-            double height = 0;
+            Address origin = new Address("123", "Random Street", "City", "State", (int)(Math.random() * 99450) + 501);
+            Address destination = new Address("123", "Random Street", "City", "State", (int)(Math.random() * 99450) + 501);
+            double weight = Math.round(((Math.random() * 50) + 2) * 100) / 100.0;
+            double length = Math.round(((Math.random() * 25) + 5) * 100) / 100.0;
+            double width = Math.round(((Math.random() * 25) + 5) * 100) / 100.0;
+            double height = Math.round(((Math.random() * 25) + 5) * 100) / 100.0;
             packages.add(new Package(origin, destination, weight, height, length, width));
         }
     }
 
-    public int generateTotalCost(){
-        int total = 0;
+    public double generateTotalCost(){
+        double total = 0;
         for (Package thePackage : packages) {
             total += PostageCalculator.calculatePostage(thePackage);
         }
+        total = Math.round(total * 100.0) / 100.0;
         return total;
     }
 
     public String getSimulationInfo() {
         String info = "";
         for (int i = 0; i < packages.size(); i++) {
-            info += "Package " + (i + 1) + "---------------------------\n";
+            info += "\nPackage " + (i + 1) + "------------------------------------------------\n";
             info += "Origin Address: " + packages.get(i).getOrigin() + "\n";
             info += "Destination Address: " + packages.get(i).getDestination() + "\n";
-            info += "Weight: " + packages.get(i).getWeight() + "\n";
-            info += "Height: " + packages.get(i).getDestination() + "\n";
-            info += "Length: " + packages.get(i).getLength() + "\n";
-            info += "Width: " + packages.get(i).getWidth() + "\n";
+            info += "Weight: " + packages.get(i).getWeight() + " pounds\n";
+            info += "Height: " + packages.get(i).getHeight() + " inches\n";
+            info += "Length: " + packages.get(i).getLength() + " inches\n";
+            info += "Width: " + packages.get(i).getWidth() + " inches\n";
         }
         return info;
     }
--- src/PackageSimulatorRunner.java
+++ src/PackageSimulatorRunner.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 public class PackageSimulatorRunner {
     public static void main(String[] args) {
-        Address test = new Address("1313", "Mockingbird Lane", "Springfield", "MO", 65123);
-        System.out.println(test);
-        System.out.println(PostageCalculator.calculatePostage(73561, 10206, 2.7, 2, 4, 6));
         Scanner scanner = new Scanner(System.in);
         String answer = "";
         while (!answer.equals("4")) {
-            System.out.println("------------------------");
+            System.out.println("------------------------------------------------");
             System.out.println("What would you like to do?");
             System.out.println("1. Calculate the price of one package");
             System.out.println("2. Simulate packages");
@@ -33,10 +30,31 @@ public class PackageSimulatorRunner {
                 double width = Double.parseDouble(scanner.nextLine());
                 System.out.println("Cost: " + PostageCalculator.calculatePostage(zip1, zip2, weight, height, length, width));
             } else if (answer.equals("2")) {
+                PackageSimulator simulator = new PackageSimulator();
                 System.out.print("How many packages would you like to simulate? ");
                 int packagesNum = Integer.parseInt(scanner.nextLine());
+                simulator.generatePackages(packagesNum);
                 System.out.print("Randomly generated packages info: ");
-
+                System.out.println(simulator.getSimulationInfo());
+                System.out.println("------------------------------------------------");
+                System.out.println("Total Cost of all packages: $" + simulator.generateTotalCost());
+            } else if (answer.equals("3")) {
+                System.out.println("The base cost of a package is $3.75");
+                System.out.println("\u001B[1mFinal costs are calculated based on 5 different attributes: origin zipcode, destination zipcode, " +
+                        "weight, height, length, and width.");
+                System.out.println("\u001B[1m\u001b[4mOrigin & Destination ZipCode:\u001b[0m");
+                System.out.println("•Each package has two zipCode; one indicating where it's coming from and the other " +
+                        "indication where it's going.");
+                System.out.println("•We take the difference between the two county codes (first 3 digits) and divide that by 100. " +
+                        "That gets added onto the final cost");
+                System.out.println("\u001B[1m\u001b[4mWeight:\u001b[0m");
+                System.out.println("•Packages have a maximum weight of 40 pounds with no extra cost");
+                System.out.println("•Any package that exceeds the 40 pound weight limit, gets charged 10 extra cents per " +
+                        "tenth of a pound that goes over");
+                System.out.println("\u001B[1m\u001b[4mHeight & Length & Width:\u001b[0m");
+                System.out.println("•Packages have a maximum size on each side of 36 inches with no extra cost");
+                System.out.println("•Any package with any side exceeding that 36 inch limit, gets charged 10 extra cents " +
+                        "per inch that goes over (All sides are tracked separately)");
             }
         }
     }
